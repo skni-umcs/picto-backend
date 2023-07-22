607bbce9028855fe9fc161bff71c5192b33204aa
mikolaj.martyna <mikolaj.martyna@skni.umcs.pl>
2023-07-22 14:24:14 +0200

    Initial Spring Boot setup

---
+++ src/test/java/pl/umcs/workshop/WorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.umcs.workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
