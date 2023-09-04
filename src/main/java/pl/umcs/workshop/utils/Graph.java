0ace9e5f23e8d08dee07cbb77108b07f343ec1fc
Osariusz <osariusz@interia.pl>
2023-09-04 12:17:47 +0200

    K-regular graph generation

--- src/main/java/pl/umcs/workshop/utils/Graph.java
+++ src/main/java/pl/umcs/workshop/utils/Graph.java
@@ -1,11 +1,9 @@
 package pl.umcs.workshop.utils;
 
+
 import pl.umcs.workshop.user.User;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Graph {
     private Map<Long, List<Long>> adjUsers = new HashMap<>();
@@ -44,14 +42,103 @@ public class Graph {
         return adjUsers.get(user.getId());
     }
 
+
+    public static Map<Integer, Integer> lowestValues(Map<Integer, Integer> values) {
+        int lowest = Integer.MAX_VALUE;
+        Map<Integer, Integer> result = new HashMap<>();
+        for (Integer key : values.keySet()) {
+            Integer value = values.get(key);
+            if(value == lowest) {
+                result.put(key, value);
+            }
+            else if(value < lowest) {
+                lowest = value;
+                result = new HashMap<>();
+                result.put(key, value);
+            }
+        }
+        return result;
+    }
+
+    public static Integer chooseRandomLowestVertexPositionWithoutTaken(Map<Integer, Integer> verticesConnectionCount, Integer currentVertex, List<Map.Entry<Integer,Integer>> pairs) {
+        Random random = new Random();
+        Set lowestValueVertices = lowestValues(verticesConnectionCount).keySet();
+        if(lowestValueVertices.contains(currentVertex)) {
+            lowestValueVertices.remove(currentVertex);
+        }
+        List<Integer> lowestValueList = new ArrayList<>();
+        lowestValueList.addAll(lowestValueVertices);
+        Integer chosenPosition = random.nextInt(lowestValueList.size());
+        chosenPosition = lowestValueList.get(chosenPosition);
+        while(lowestValueList.size() > 0) {
+            if(pairPresent(pairs,currentVertex,chosenPosition) || chosenPosition == currentVertex) {
+                lowestValueList.remove(chosenPosition);
+                chosenPosition = random.nextInt(lowestValueList.size());
+                chosenPosition = lowestValueList.get(chosenPosition);
+            }
+            else {
+                return chosenPosition;
+            }
+        }
+        return -1;
+
+    }
+
+    public static void modifyConnectionCount(Map<Integer,Integer> connectionCount, Integer vertex, Integer value) {
+        connectionCount.put(vertex,connectionCount.get(vertex)+value);
+    }
+
+    public static boolean pairPresent(List<Map.Entry<Integer,Integer>> pairs, Integer a, Integer b) {
+        for(Map.Entry<Integer, Integer> pair : pairs) {
+            if(pair.getKey() == a || pair.getKey() == b) {
+                if(pair.getValue() == a || pair.getValue() == b) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static List<Map.Entry<Integer, Integer>> generateGraph(int k, int n) {
+        List<Map.Entry<Integer, Integer>> result = new ArrayList<>();
+        Map<Integer, Integer> verticesConnectionCount = new HashMap<>();
+        for(int i = 0;i<n;++i) {
+            verticesConnectionCount.put(i,0);
+        }
+        for(int i = 0;i<n;++i) {
+            Integer lowestVertexPosition = chooseRandomLowestVertexPositionWithoutTaken(verticesConnectionCount, null, result);
+            if(verticesConnectionCount.get(lowestVertexPosition) >= k) {
+                continue;
+            }
+
+            Integer anotherLowestVertex = chooseRandomLowestVertexPositionWithoutTaken(verticesConnectionCount, lowestVertexPosition, result);
+            if(verticesConnectionCount.get(anotherLowestVertex) >= k) {
+                break;
+            }
+            while(verticesConnectionCount.get(lowestVertexPosition) < k && verticesConnectionCount.get(anotherLowestVertex) < k) {
+                modifyConnectionCount(verticesConnectionCount,lowestVertexPosition,1);
+                modifyConnectionCount(verticesConnectionCount,anotherLowestVertex,1);
+                result.add(Map.entry(lowestVertexPosition, anotherLowestVertex));
+                anotherLowestVertex = chooseRandomLowestVertexPositionWithoutTaken(verticesConnectionCount, lowestVertexPosition, result);
+            }
+            verticesConnectionCount.remove(lowestVertexPosition);
+
+        }
+        return result;
+    }
+
     public void printGraph() {
-        for (Long userId : adjUsers.keySet()) {
+        List<Map.Entry<Integer, Integer>> result = generateGraph(3,30);
+        for(Map.Entry<Integer, Integer> pair : result) {
+            System.out.println(String.format("(%d,%d),",pair.getKey(),pair.getValue()));
+        }
+        /*for (Long userId : adjUsers.keySet()) {
             System.out.print(userId + ": ");
 
             for (Long neighId : adjUsers.get(userId)) {
                 System.out.print(neighId + " ");
             }
             System.out.println();
-        }
+        }*/
     }
 }
