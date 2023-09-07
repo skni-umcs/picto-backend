75b28dfc2e830cb67ba7cdf5a2da3b794022210b
mikolaj.martyna <mikolaj.martyna@skni.umcs.pl>
2023-09-07 11:09:19 +0200

    Fix CORS policy

--- src/main/java/pl/umcs/workshop/utils/WebConfig.java
+++ src/main/java/pl/umcs/workshop/utils/WebConfig.java
@@ -1,5 +1,8 @@
 package pl.umcs.workshop.utils;
 
+import org.jetbrains.annotations.NotNull;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -9,7 +12,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+    public void addCorsMappings(@NotNull CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:3000/").allowedHeaders("*");
     }
 }
