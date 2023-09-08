25485d654d98d678be5423c1e6124205b7b19bc7
Osariusz <osariusz@interia.pl>
2023-09-08 16:58:19 +0200

    RoundRepository fix

--- src/main/java/pl/umcs/workshop/round/RoundRepository.java
+++ src/main/java/pl/umcs/workshop/round/RoundRepository.java
@@ -2,15 +2,19 @@ package pl.umcs.workshop.round;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import pl.umcs.workshop.game.Game;
+import pl.umcs.workshop.user.User;
 
 import java.util.List;
 
 public interface RoundRepository extends JpaRepository<Round, Long> {
 
-    Round findOneByGameIdAndUserOneIdAndUserTwoIdAndGeneration(Long gameId, Long userOneId, Long userTwoId, int generation);
+    Round findOneByGameIdAndUserOneIdAndGeneration(Long gameId, Long userOneId, int generation);
+    Round findOneByGameIdAndUserTwoIdAndGeneration(Long gameId, Long userTwoId, int generation);
 
     default Round getNextRound(Long gameId, Long userId, int generation) {
-        return findOneByGameIdAndUserOneIdAndUserTwoIdAndGeneration(gameId, userId, userId, generation);
+        Round b = findOneByGameIdAndUserOneIdAndGeneration(gameId, userId, generation);
+        Round c = findOneByGameIdAndUserTwoIdAndGeneration(gameId, userId, generation);
+        return b == null ? c : b;
     }
 
     List<Round> findAllByGame(Game game);
