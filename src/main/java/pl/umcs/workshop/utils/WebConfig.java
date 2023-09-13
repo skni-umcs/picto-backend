4414c67c6adbe0c259175f4eca2f7d2b1fd0de89
mmartyna <enix1701@gmail.com>
2023-09-13 12:42:52 +0200

    Fix CORS

--- src/main/java/pl/umcs/workshop/utils/WebConfig.java
+++ src/main/java/pl/umcs/workshop/utils/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(@NotNull CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000/")
+        .allowedOrigins("https://pictodev.skni.umcs.pl/")
         .allowedHeaders("*")
         .allowCredentials(true);
   }
