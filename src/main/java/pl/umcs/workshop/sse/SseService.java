eef35a9a92c5db2e863f4a67a624af1bfd75761c
mmartyna <enix1701@gmail.com>
2023-09-16 18:34:13 +0200

    Fix: ConcurrentModificationException when ending game

--- src/main/java/pl/umcs/workshop/sse/SseService.java
+++ src/main/java/pl/umcs/workshop/sse/SseService.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+import java.util.Set;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -63,7 +64,9 @@ public class SseService {
   }
 
   public static void closeSseConnectionForAll(Long gameId) {
-    for (Map.Entry<Long, SseEmitter> entry : userSessions.entrySet()) {
+    Set<Map.Entry<Long, SseEmitter>> userSessionEntries = userSessions.entrySet();
+
+    for (Map.Entry<Long, SseEmitter> entry : userSessionEntries) {
       Long userId = entry.getKey();
       User user = userService.getUser(userId);
 
