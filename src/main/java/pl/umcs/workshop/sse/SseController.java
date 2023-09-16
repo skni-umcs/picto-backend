de22178eb8a0cb53f2365a72c667dc04e72d0c1f
mmartyna <enix1701@gmail.com>
2023-09-16 18:36:42 +0200

    Sse token as query param

--- src/main/java/pl/umcs/workshop/sse/SseController.java
+++ src/main/java/pl/umcs/workshop/sse/SseController.java
@@ -1,14 +1,15 @@
 package pl.umcs.workshop.sse;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
 @RestController
 public class SseController {
-  @GetMapping("event")
-  public SseEmitter handleSseConnection(@RequestHeader("x-session") String token) {
+  @GetMapping("event?token={token}")
+  public SseEmitter handleSseConnection(@PathVariable String token) {
     return SseService.handleSseConnection(token);
   }
 }
